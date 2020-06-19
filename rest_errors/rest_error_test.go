25f5b390d8cc65823203f50458475106047cda95
Santiago Correa <sampado@gmail.com>
2020-06-19 18:01:38 -0400

    Fixed unit tests

--- rest_errors/rest_error_test.go
+++ rest_errors/rest_error_test.go
@@ -12,29 +12,29 @@ func TestNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("my message", errors.New("database error"))
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "my message", err.Message)
-	assert.EqualValues(t, "Internal Server Error", err.Error)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "my message", err.Message())
+	assert.EqualValues(t, "message: my message - status: 500 - error: Internal Server Error - causes: [[database error]]", err.Error())
 
 	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "database error", err.Causes[0])
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "database error", err.Causes()[0])
 }
 
 func TestNewNotFoundError(t *testing.T) {
 	err := NewNotFoundError("my message")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotFound, err.Status)
-	assert.EqualValues(t, "my message", err.Message)
-	assert.EqualValues(t, "Not Found", err.Error)
+	assert.EqualValues(t, http.StatusNotFound, err.Status())
+	assert.EqualValues(t, "my message", err.Message())
+	assert.EqualValues(t, "message: my message - status: 404 - error: Not Found - causes: [[]]", err.Error())
 }
 
 func TestNewBadRequestError(t *testing.T) {
 	err := NewBadRequestError("my message")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusBadRequest, err.Status)
-	assert.EqualValues(t, "my message", err.Message)
-	assert.EqualValues(t, "Bad Request", err.Error)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "my message", err.Message())
+	assert.EqualValues(t, "message: my message - status: 400 - error: Bad Request - causes: [[]]", err.Error())
 }
